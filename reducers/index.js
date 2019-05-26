9b0d44cb792d8924af9676f0ad0a4c4c981e798d
Jose Cerqueira <jose@origensmedia.com>
2019-05-26 09:48:14 +0100

    Set up the quiz reducer

--- reducers/index.js
+++ reducers/index.js
@@ -3,11 +3,13 @@ import { combineReducers } from "redux";
 import loadingReducer from "./loadingReducer";
 import decksReducer from "./decksReducers";
 import errorReducer from "./errorReducer";
+import quizReducer from "./quizReducer";
 
 const rootReducer = combineReducers({
 	isLoading: loadingReducer,
 	decks: decksReducer,
-	error: errorReducer
+	error: errorReducer,
+	quiz: quizReducer
 });
 
 export default rootReducer;
